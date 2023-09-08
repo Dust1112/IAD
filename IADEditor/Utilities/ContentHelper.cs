f7ae52c8b40ada6165dfeab66c9baa044ea10851
Alexandru Iancu <49159531+Dust1112@users.noreply.github.com>
2023-09-08 22:49:23 +0100

    feat: IAD-19 unpack geometry data

---
+++ IADEditor/Utilities/ContentHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+namespace IADEditor.Utilities;
+
+public static class ContentHelper
+{
+    public static string GetRandomString(int length = 8)
+    {
+        if (length <= 0) { length = 8; }
+
+        var n = length / 11;
+        var sb = new StringBuilder();
+        for (int i = 0; i <= n; ++i)
+        {
+            sb.Append(Path.GetRandomFileName().Replace(".", ""));   
+        }
+
+        return sb.ToString(0, length);
+    }
+}
