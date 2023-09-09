fdde6a8359bab6ed990e5f2a794d6fae2a601ddc
Alexandru Iancu <49159531+Dust1112@users.noreply.github.com>
2023-09-09 02:11:57 +0100

    feat: IAD-20 refactor and create primitive mesh dialog

--- IADEditor/Editors/WorldEditor/WorldEditorView.xaml.cs
+++ IADEditor/Editors/WorldEditor/WorldEditorView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using IADEditor.Content;
 using IADEditor.GameDev;
 
 namespace IADEditor.Editors
@@ -37,6 +26,12 @@ namespace IADEditor.Editors
         {
             NewScriptDialog dlg = new NewScriptDialog();
             dlg.ShowDialog();
+        }
+
+        private void OnCreatePrimitiveMesh_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new PrimitiveMeshDialog();
+            dialog.ShowDialog();
         }
     }
 }
